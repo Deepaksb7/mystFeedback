63fa39b132fa27baa857d357ed0c6710c4411d51
Deepaksb7 <beharadeepak6@gmail.com>
2025-09-20 23:22:38 +0530

    sign in page completed

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,9 +1,101 @@
-import React from 'react'
+"use client"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import Link from "next/link"
+import { useDebounceValue,useDebounceCallback } from 'usehooks-ts'
+import axios, { AxiosError } from "axios"
+import React, { useEffect, useState } from 'react'
+import { useRouter } from "next/navigation"
+import { signupSchema } from "@/schemas/signupSchema"
+import { ApiResponse } from "@/types/apiResponse"
+import { toast } from "sonner"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { LoaderCircle } from 'lucide-react';
+import { signinSchema } from "@/schemas/signinSchema"
+import { signIn } from "next-auth/react"
+import Credentials from "next-auth/providers/credentials"
 
 const page = () => {
+  const [isSubmiting, setIsSubmiting] = useState(false)
+  const router = useRouter()
+
+  const form = useForm<z.infer<typeof signinSchema>>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      identifier: "",
+      password: ''
+    }
+  })
+
+  const onSubmit = async (data: z.infer<typeof signinSchema>) => {
+    const result = await signIn('Credentials',{
+      redirect:false,
+      identifier:data.identifier,
+      password:data.password
+    })
+    if(result?.error){
+      toast.error("Incorrect username or password")
+    }
+
+    if(result?.url){
+      router.replace('/dashboard')
+    }
+  }
+
   return (
-    <div>
-      Sign in page
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">Join Mystery Message</h1>
+          <p className="mb-4">Sign in to start your anonymous adventure</p>
+        </div>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                name="identifier"
+                control={form.control}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email" {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="Password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" >
+                Sign in
+              </Button>
+            </form>
+          </Form>
+          <div className="text-center mt-4">
+            <p>
+              Already a member?{" "}
+              <Link href={"/signin"} className="text-blue-600 hover:text-blue-800">Sign in</Link>
+            </p>
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
